3c3af5eb958380da38dc8641ed5b0e567fe2751c
Horiodino <holiodin@gmail.com>
2023-06-22 21:39:21 +0530

    added : network security group

--- main.tf
+++ main.tf
@@ -31,6 +31,39 @@ resource "azurerm_subnet" "shared-subnet" {
     address_prefixes = [" "]
 }
 
+resource "azurerm_network_security_group" "security-group" {
+    name = "kube-cluster-nsg"
+    location = azurerm_resource_group.name.location
+    resource_group_name = azurerm_resource_group.name.name
+    security_rule {
+        name = "kube-cluster-nsg-rule"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "80"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+
+resource "azurerm_network_interface" "virtual-nic" {
+    name = "kube-cluster-nic"
+    location = azurerm_resource_group.name.location
+    resource_group_name = azurerm_resource_group.name.name
+
+    ip_configuration {
+        name = "kube-cluster-nic-ip"
+        subnet_id = azurerm_subnet.name.id
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id = azurerm_public_ip.name.id
+    }
+}
+
+
+
 
 
 // output
