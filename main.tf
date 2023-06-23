995d9d185d50905d7f890ae68a52cc44480a7d8c
Horiodino <holiodin@gmail.com>
2023-06-23 21:14:14 +0530

    fixed some errors and typos

--- main.tf
+++ main.tf
@@ -4,159 +4,132 @@
 //                      :: the vm going to communicate with each other so for all the vm we need to share the same network configuration
 //                     :: if you are using in local machine then you ndoesnot need to create the network configuration because you vms are using
 //                    :: the same network configuration as local machine
-
-
+//
+// Provider azurerm
 provider "azurerm" {
-    features {}
+  features {}
 }
 
-// resource group
-resource "azurerm_resource_group" "name" {
-    name = "kube-cluster"
-    location = "East US"
+// Resource group
+resource "azurerm_resource_group" "home-lab" {
+  name     = "kube-cluster"
+  location = "eastus"
 }
 
-// network configuration
-resource "azurerm_virtual_network" "name" {
-    name = "kube-cluster-vnet"
-    address_space = ["  "]
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
+// Network configuration
+resource "azurerm_virtual_network" "vnet" {
+  name                = "kube-cluster-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
 }
 
 resource "azurerm_subnet" "shared-subnet" {
-    name = "kube-cluster-subnet"
-    resource_group_name = azurerm_resource_group.name.name
-    virtual_network_name = azurerm_virtual_network.name.name
-    address_prefixes = [" "]
+  name                 = "kube-cluster-subnet"
+  resource_group_name  = azurerm_resource_group.home-lab.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_network_security_group" "security-group" {
-    name = "kube-cluster-nsg"
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
-    security_rule {
-        name = "kube-cluster-nsg-rule"
-        priority = 1001
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "80"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
-    }
+  name                = "kube-cluster-nsg"
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
+
+  security_rule {
+    name                       = "kube-cluster-nsg-rule"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  // Add SSH rule
+  security_rule {
+    name                       = "ssh-rule"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-
-resource "azurerm_network_interface" "virtual-nic" {
-    name = "kube-cluster-nic"
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
-
-    ip_configuration {
-        name = "kube-cluster-nic-ip"
-        subnet_id = azurerm_subnet.name.id
-        private_ip_address_allocation = "dynamic"
-        public_ip_address_id = azurerm_public_ip.name.id
-    }
+resource "azurerm_subnet_network_security_group_association" "subnet-nsg-association" {
+  subnet_id                 = azurerm_subnet.shared-subnet.id
+  network_security_group_id = azurerm_network_security_group.security-group.id
 }
 
-
-resource "azurerm_public_ip" "public-ip" {
-    name = "kube-cluster-pip"
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
-    allocation_method = "Dynamic"
+resource "azurerm_public_ip" "lb-public-ip" {
+  name                = "kube-cluster-lb-public-ip"
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
+  allocation_method   = "Dynamic"
 }
 
+resource "azurerm_network_interface" "nic" {
+  name                = "kube-cluster-nic"
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
 
-
-resource "azurerm_lb" "load-balancer" {
-    name = "kube-cluster-lb"
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
-    sku = "Standard"
+  ip_configuration {
+    name                          = "kube-cluster-nic-ip"
+    subnet_id                     = azurerm_subnet.shared-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lb-public-ip.id
+  }
 }
 
-// i dont know how to explain it  but here is the explanation from the web
-# By configuring backend address pools, you can define the set of resources that will receive the traffic load balanced by the Azure Load Balancer,
-#  allowing for efficient distribution and scaling of network traffic across your application infrastructure.
-resource "azurerm_lb_backend_address_pool" "backend-pool" {
-    name = "kube-cluster-lb-pool"
-    resource_group_name = azurerm_resource_group.name.name
-    loadbalancer_id = azurerm_lb.name.id
-}
-
-
-// it is used to check the health of the vms 
-resource "azurerm_lb_probe" "health-check-probe" {
-    name = "kube-cluster-lb-probe"
-    resource_group_name = azurerm_resource_group.name.name
-    loadbalancer_id = azurerm_lb.name.id
-    port = 80
-    protocol = "TCP"
-}
-
-
-// load balancer rule :: it is used to distribute the traffic across the vms simply a load balancer rule is used to distribute the traffic across the vms
-resource "azurerm_lb_rule" "load-balancer-rule" {
-    name = "kube-cluster-lb-rule"
-    resource_group_name = azurerm_resource_group.name.name
-    loadbalancer_id = azurerm_lb.name.id
-    backend_address_pool_id = azurerm_lb_backend_address_pool.name.id
-    backend_port = 80
-    frontend_ip_configuration_id = azurerm_lb.name.frontend_ip_configuration[0].id
-    frontend_port = 80
-    protocol = "TCP"
-    probe_id = azurerm_lb_probe.name.id
-}
-
-
-
-// in simple words it is used to distribute the vms across the different hardware like if one hardware is down then it will not affect the other hardware
 resource "azurerm_availability_set" "availability-set" {
-    name = "kube-cluster-availability-set"
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
+  name                = "kube-cluster-availability-set"
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
+  platform_fault_domain_count   = 2
+  platform_update_domain_count = 2
 }
 
-
-resource "azurerm_virtual_machine" "virtual-machine" {
-    name = "kube-cluster-vm"
-    location = azurerm_resource_group.name.location
-    resource_group_name = azurerm_resource_group.name.name
-    network_interface_ids = [azurerm_network_interface.name.id]
-    vm_size = "Standard_B2s"
-    availability_set_id = azurerm_availability_set.name.id
-
-    // delete os disk and data disk on termination when you delete the vm then it will delete the os disk and data disk also
-    delete_os_disk_on_termination = true
-    delete_data_disks_on_termination = true
-    storage_image_reference {
-        publisher = "Canonical"
-        offer = "UbuntuServer"
-        sku = "16.04-LTS"
-        version = "latest"
-    }
-    storage_os_disk {
-        name = "kube-cluster-os-disk"
-        caching = "ReadWrite"
-        create_option = "FromImage"
-        managed_disk_type = "Standard_LRS"
-    }
-    os_profile {
-        computer_name = "kube-cluster-vm"
-    }
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
-    tags = {
-        environment = "dev"
-    }
+resource "azurerm_lb" "lb" {
+  name                = "kube-cluster-lb"
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
+  sku                 = "Basic"
 }
 
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "kube-cluster-vm"
+  location            = azurerm_resource_group.home-lab.location
+  resource_group_name = azurerm_resource_group.home-lab.name
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  disable_password_authentication = true
 
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+  availability_set_id = azurerm_availability_set.availability-set.id
 
-// output
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "kube-cluster-os-disk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
