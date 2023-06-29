8331ab3ccab328f2a6c9d464e391f5a89b501357
Horiodino <holiodin@gmail.com>
2023-06-29 07:37:40 +0530

    setting up compute resources

--- kubernetes-hardway-aws/seetup.sh
+++ kubernetes-hardway-aws/seetup.sh
@@ -1,22 +1,59 @@
+# Create VPC (Virtual Private Cloud)
+
 VPC_ID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output text --query 'Vpc.VpcId')
 aws ec2 create-tags --resources ${VPC_ID} --tags Key=Name,Value=kubernetes-the-hard-way
 aws ec2 modify-vpc-attribute --vpc-id ${VPC_ID} --enable-dns-support '{"Value": true}'
 aws ec2 modify-vpc-attribute --vpc-id ${VPC_ID} --enable-dns-hostnames '{"Value": true}'
 
 
-// the above command will return the VPC ID, which we will use in the next command to create subnets
-// vpc is nothing but a isolated network in AWS like if you heard about azure resource group, it is same as that
+# the above command will return the VPC ID, which we will use in the next command to create subnets
+# vpc is nothing but a isolated network in AWS like if you heard about azure resource group, it is same as that
 
-// Create Subnets
+# Create Subnets
 SUBNET_ID=$(aws ec2 create-subnet \
   --vpc-id ${VPC_ID} \
   --cidr-block 10.0.1.0/24 \
   --output text --query 'Subnet.SubnetId')
 aws ec2 create-tags --resources ${SUBNET_ID} --tags Key=Name,Value=kubernetes
 
-// the above script will create a subnet in the VPC we created in the previous step
-//  a subnet is used to divide the network into smaller networks. 
-// the cidr block is the ip range of the subnet, which is private ip range
-// and lastly -- output text --query 'Subnet.SubnetId' will return the subnet id, which we will use in the next command
+# the above script will create a subnet in the VPC we created in the previous step
+#  a subnet is used to divide the network into smaller networks. 
+# the cidr block is the ip range of the subnet, which is private ip range
+# and lastly -- output text --query 'Subnet.SubnetId' will return the subnet id, which we will use in the next command
 
+# Create Internet Gateway
+INTERNET_GATEWAY_ID=$(aws ec2 create-internet-gateway --output text --query 'InternetGateway.InternetGatewayId')
+aws ec2 create-tags --resources ${INTERNET_GATEWAY_ID} --tags Key=Name,Value=kubernetes
+aws ec2 attach-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID} --vpc-id ${VPC_ID}
+
+# the above script will create a internet gateway and attach it to the VPC we created in the previous step
+# a internet gateway is used to connect the VPC to the internet and allow internet access to the resources in the VPC
+
+# Create Route Table
+ROUTE_TABLE_ID=$(aws ec2 create-route-table --vpc-id ${VPC_ID} --output text --query 'RouteTable.RouteTableId')
+aws ec2 create-tags --resources ${ROUTE_TABLE_ID} --tags Key=Name,Value=kubernetes
+aws ec2 associate-route-table --route-table-id ${ROUTE_TABLE_ID} --subnet-id ${SUBNET_ID}
+aws ec2 create-route --route-table-id ${ROUTE_TABLE_ID} --destination-cidr-block 0.0.0.0/0 --gateway-id ${INTERNET_GATEWAY_ID}
+
+# the above script will create a route table and associate it with the subnet we created in the previous step
+# a route table is used to route the traffic from the subnet to the internet gateway
+# the destination cidr block is the ip range of the traffic that will be routed to the internet gateway
+
+# Create Firewall 
+SECURITY_GROUP_ID=$(aws ec2 create-security-group \
+  --group-name kubernetes \
+  --description "Kubernetes security group" \
+  --vpc-id ${VPC_ID} \
+  --output text --query 'GroupId')
+aws ec2 create-tags --resources ${SECURITY_GROUP_ID} --tags Key=Name,Value=kubernetes
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol all --cidr 10.0.0.0/16
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol all --cidr 10.200.0.0/16
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 6443 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 443 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol icmp --port -1 --cidr 0.0.0.0/0
+
+# the above script will create a firewall and allow all traffic from the subnet to the firewall
+# a firewall is used to control the traffic that is allowed to enter or leave the subnet
+# the cidr block is the ip range of the traffic that will be allowed to enter or leave the subnet
 
