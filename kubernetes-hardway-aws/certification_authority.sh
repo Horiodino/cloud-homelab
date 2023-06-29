18d45ec7ac3dcadfb09026ece43333c0e19ec548
Horiodino <holiodin@gmail.com>
2023-06-29 08:05:48 +0530

    setting up compute resources: worker and control-plane

---
+++ kubernetes-hardway-aws/certification_authority.sh
@@ -0,0 +1,13 @@
+# Certificate Authority
+# why we need a CA?
+# 
+#  we need a CA to generate the certificates for the kubernetes cluster
+#  the certificates are used to authenticate the components of the kubernetes cluster
+#  encrypt the communication between the components of the kubernetes cluster
+#  to sign the tokens used by the kubernetes cluster
+#  to authenticate the users of the kubernetes cluster
+#  and they are used to sign the kubeconfig files used by the users of the kubernetes cluster
+
+# its the most important part of the kubernetes cluster , which can also lead to
+#  security issues if not handled properly
+
